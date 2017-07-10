6135b0025c29aa9af5f52240811eeabe92438262
TJ Usiyan <griotspeak@gmail.com>
2017-07-09 23:14:17 -0400

    Initial Commit

---
+++ Digitalpha/Digitalpha.h
@@ -0,0 +1,19 @@
+//
+//  Digitalpha.h
+//  Digitalpha
+//
+//  Created by TJ Usiyan on 2017/07/09.
+//  Copyright © 2017 Buttons and Lights LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Digitalpha.
+FOUNDATION_EXPORT double DigitalphaVersionNumber;
+
+//! Project version string for Digitalpha.
+FOUNDATION_EXPORT const unsigned char DigitalphaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Digitalpha/PublicHeader.h>
+
+
